38ed85eea56b8779cad2312a79da9482b17c4061
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-06 13:51:26 -0700

    Progress with plot

--- static/js/app.js
+++ static/js/app.js
@@ -40,15 +40,43 @@ Display each key-value pair from the metadata JSON object somewhere on the page.
 
 const URL = " https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-const data = await d3.json(URL)
+let dataF
+
+d3.json(URL).then((data) => {
+    dataF = data
+    console.log(data)
+    const ids = data.names
+    ids.forEach(id => document.getElementById("selDataset").appendChild(new Option(`${id}`, Number(id))))
 
 
-let sample_values 
-let otu_ids 
-let otu_labels 
+    const {
+        sample_values,
+        otu_ids,
+        otu_labels
+    } = data.samples[0]
+    // console.log(data, sample_values, otu_ids, otu_labels)
+    Plotly.newPlot("plot", [{
+        type: "bar",
+        y: sample_values,
+        x: otu_ids,
+        text: sample_values,
+        hovertext: otu_labels
+    }])
 
-const otherData = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
 
-  Plotly.newPlot("plot", data);
+
+
+})
+
+
+
+
+const optionChanged = (id) => {
+console.log(dataF)
+const sampleIndex = dataF.names.findIndex((element)=> element == id)
+    let { sample_values: data, otu_labels: labels, otu_ids: ids } = dataF.samples[sampleIndex]
+   
+    Plotly.restyle("plot", "x",[otu_ids])
+    Plotly.restyle("plot", "y", [sample_values])
+    Plotly.restyle("plot", "hovertext", [otu_labels])
+}
