750fb9b82e51defdbdd73719ba1b3acc3fdef221
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-06 11:56:21 -0700

    data

---
+++ Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,48 @@
+//
+/**
+Use the D3 library to read in samples.json from the URL https://2u-data-curriculu
+m-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-
+Homework/samples.json.
+
+Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found
+in that individual.
+
+Use sample_values as the values for the bar chart.
+
+Use otu_ids as the labels for the bar chart.
+
+Use otu_labels as the hovertext for the chart.
+
+bar Chart
+
+Create a bubble chart that displays each sample.
+
+Use otu_ids for the x values.
+
+Use sample_values for the y values.
+
+Use sample_values for the marker size.
+
+Use otu_ids for the marker colors.
+
+Use otu_labels for the text values.
+Bubble Chart
+
+Display the sample metadata, i.e., an individual's demographic information.
+
+Display each key-value pair from the metadata JSON object somewhere on the page. 
+
+
+
+
+
+ */
+
+const URL = " https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+const data = d3.json(URL).then(()=>data=> data).catch((err)=>console.error(err))
+
+let sample_values 
+let otu_ids 
+let otu_labels 
+
